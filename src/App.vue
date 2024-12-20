63f0804876b4889af3a948432ea39de6705d2569
Venki8500 <cherryvenki8500@gmail.com>
2024-12-20 17:54:24 +0530

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Countries from './components/Countries.vue';
 import Navbar from './components/Navbar.vue';
+import Products from './components/Products.vue';
 
 
 </script>
 
 <template>
   <Navbar></Navbar>
-  <Countries></Countries>
+  <!-- <Countries></Countries> -->
+   <Products></Products>
 
 </template>
